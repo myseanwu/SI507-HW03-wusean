fac068c5b896adce187cd956b9f2d5c3898f2ecb
SEAN WU <SEANWU@0587349974.wireless.umich.net>
2019-09-18 18:46:43 -0400

    adding new code

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,8 @@
 
 answer = input('What is your question?')
+
+    if not answer.endswith('?'):
+        print("I’m sorry, I can only answer questions.")
+        return answer
+        if answer = 'quit':
+            break
