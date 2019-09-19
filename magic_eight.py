1c710624d50e9505c3dfed110070a73eea1014e2
SEAN WU <SEANWU@Sean-Wu.hsd1.mi.comcast.net>
2019-09-19 01:53:47 -0400

    update the code again

--- magic_eight.py
+++ magic_eight.py
@@ -8,10 +8,10 @@ def answer():
                "Most likely.","Outlook good.", "Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.",
                 "Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
 
-                if answer == 'quit':
-                    break
-                if not answer.endswith('?'):
-                    print("I’m sorry, I can only answer questions.")
-                elif answer.endswith('?'):
-                    print(random.choice(reply2))
-                    break
+        if answer == 'quit':
+            break
+        if not answer.endswith('?'):
+            print("I’m sorry, I can only answer questions.")
+        elif answer.endswith('?'):
+            print(random.choice(reply2))
+            break
